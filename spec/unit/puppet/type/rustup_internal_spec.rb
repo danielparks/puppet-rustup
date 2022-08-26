398fac0607627da9cfef4a074ae3ef03eb419d23
Daniel Parks <dp+git@oxidized.org>
2022-08-25 20:59:17 -0700

    Simplify names of tests.

--- spec/unit/puppet/type/rustup_internal_spec.rb
+++ spec/unit/puppet/type/rustup_internal_spec.rb
@@ -3,50 +3,50 @@
 require 'spec_helper'
 
 RSpec.describe Puppet::Type.type(:rustup_internal) do
-  it "should load" do
+  it "loads" do
     expect(described_class).not_to be_nil
   end
 
-  it "should be able to create a trivial instance" do
+  it "creates a trivial instance" do
     expect(described_class.new(:name => "user")).to_not be_nil
   end
 
-  it "should fail with a nil name" do
+  it "fails with a nil name" do
     expect { described_class.new(:name => nil) }
       .to raise_error(Puppet::Error, /Title or name must be provided/)
   end
 
-  it "should fail with a bad ensure" do
+  it "fails with a bad ensure" do
     expect { described_class.new(:name => "user", :ensure => "dfasdf") }
       .to raise_error(Puppet::Error, /Valid values are present, latest, absent/)
   end
 
-  it "should have default modify_path" do
+  it "has a default for modify_path" do
     expect(described_class.new(:name => "user")[:modify_path])
       .to eq(true)
   end
 
-  it "should accept modify_path" do
+  it "accepts modify_path" do
     expect(described_class.new(:name => "user", :modify_path => false)[:modify_path])
       .to eq(false)
   end
 
-  it "should fail with a blank installer_source" do
+  it "fails with a blank installer_source" do
     expect { described_class.new(:name => "user", :installer_source => "") }
       .to raise_error(Puppet::Error, /Installer source must not be blank/)
   end
 
-  it "should fail with a bad installer_source" do
+  it "fails with a bad installer_source" do
     expect { described_class.new(:name => "user", :installer_source => "s a as") }
       .to raise_error(Puppet::Error, /Installer source must be a valid URL/)
   end
 
-  it "should work with a good installer_source" do
+  it "works with a good installer_source" do
     expect(described_class.new(:name => "user", :installer_source => "http://localhost/foo"))
       .to_not be_nil
   end
 
-  it "should have default installer_source" do
+  it "has a default for installer_source" do
     expect(described_class.new(:name => "user")[:installer_source])
       .to eq("https://sh.rustup.rs")
   end
