aed6603ba9ef04a37f42d716a1d71cca4a8efa4d
Daniel Parks <dp+git@oxidized.org>
2022-08-25 20:32:26 -0700

    Clean up unit tests slightly.

--- spec/unit/puppet/provider/rustup_internal/shell_spec.rb
+++ spec/unit/puppet/provider/rustup_internal/shell_spec.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'puppet/provider/rustup_internal/shell'
 
-RSpec.describe 'the rustup_internal/shell provider' do
+RSpec.describe Puppet::Type.type(:rustup_internal).provide(:shell) do
   it 'loads' do
-    expect(Puppet::Type.type(:rustup_internal).provide(:shell)).not_to be_nil
+    expect(described_class).not_to be_nil
   end
 end
--- spec/unit/puppet/type/rustup_internal_spec.rb
+++ spec/unit/puppet/type/rustup_internal_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'puppet/type/rustup_internal'
 
 RSpec.describe Puppet::Type.type(:rustup_internal) do
   it "should load" do
