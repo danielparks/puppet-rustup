4f2bc68cea9320c7a900f338dd7d8cac51de24a0
Daniel Parks <dp+git@oxidized.org>
2022-08-26 17:02:24 -0700

    Fix provider testing.

--- spec/unit/puppet/provider/rustup_internal/shell_spec.rb
+++ spec/unit/puppet/provider/rustup_internal/shell_spec.rb
@@ -2,8 +2,21 @@
 
 require 'spec_helper'
 
-RSpec.describe Puppet::Type.type(:rustup_internal).provide(:shell) do
-  it 'loads' do
-    expect(described_class).not_to be_nil
+RSpec.describe Puppet::Type.type(:rustup_internal).provider(:shell) do
+  it "should refuse to load instances" do
+    expect { described_class.instances }
+      .to raise_error(RuntimeError, /Cannot query rustup installations/)
+  end
+
+  let :resource do
+    Puppet::Type.type(:rustup_internal).new(:title => "root", :provider => :shell)
+  end
+
+  let :provider do
+    described_class.new(resource)
+  end
+
+  it "has correct rustup_home" do
+    expect(provider.rustup_home).to eq(File.expand_path("~root/.rustup"))
   end
 end
