af0a03c19bfe59809e254874a6ba93368ea5cfcf
Daniel Parks <dp+git@oxidized.org>
2022-08-29 03:41:08 -0700

    Use title instead of name to create resources in unit tests.

--- spec/unit/puppet/type/rustup_internal_spec.rb
+++ spec/unit/puppet/type/rustup_internal_spec.rb
@@ -19,63 +19,63 @@ RSpec.describe Puppet::Type.type(:rustup_internal) do
     end
   end
 
-  it 'fails with a nil name' do
-    expect { described_class.new(name: nil) }
+  it 'fails with a nil title' do
+    expect { described_class.new(title: nil) }
       .to raise_error(Puppet::Error, %r{Title or name must be provided})
   end
 
-  it 'fails with a blank name' do
-    expect { described_class.new(name: '') }
+  it 'fails with a blank title' do
+    expect { described_class.new(title: '') }
       .to raise_error(Puppet::Error, %r{User is required to be a non-empty string})
   end
 
   it 'fails with a bad ensure' do
-    expect { described_class.new(name: 'user', ensure: 'dfasdf') }
+    expect { described_class.new(title: 'user', ensure: 'dfasdf') }
       .to raise_error(Puppet::Error, %r{Valid values are present, latest, absent})
   end
 
   it 'accepts modify_path' do
-    expect(described_class.new(name: 'user', modify_path: false)[:modify_path])
+    expect(described_class.new(title: 'user', modify_path: false)[:modify_path])
       .to eq(false)
   end
 
   it 'fails with a relative rustup_home' do
-    expect { described_class.new(name: 'user', rustup_home: 'dfasdf') }
+    expect { described_class.new(title: 'user', rustup_home: 'dfasdf') }
       .to raise_error(Puppet::Error, %r{Rustup home must be an absolute path})
   end
 
   it 'fails with a nil rustup_home' do
-    expect { described_class.new(name: 'user', rustup_home: nil) }
+    expect { described_class.new(title: 'user', rustup_home: nil) }
       .to raise_error(Puppet::Error, %r{Got nil value for rustup_home})
   end
 
   it 'works with an absolute rustup_home' do
-    expect(described_class.new(name: 'user', rustup_home: '/opt/rustup'))
+    expect(described_class.new(title: 'user', rustup_home: '/opt/rustup'))
       .not_to be_nil
   end
 
   it 'fails with a number installer_source' do
-    expect { described_class.new(name: 'user', installer_source: 3) }
+    expect { described_class.new(title: 'user', installer_source: 3) }
       .to raise_error(Puppet::Error, %r{Installer source must be a valid URL})
   end
 
   it 'fails with a nil installer_source' do
-    expect { described_class.new(name: 'user', installer_source: nil) }
+    expect { described_class.new(title: 'user', installer_source: nil) }
       .to raise_error(Puppet::Error, %r{Got nil value for installer_source})
   end
 
   it 'fails with a blank installer_source' do
-    expect { described_class.new(name: 'user', installer_source: '') }
+    expect { described_class.new(title: 'user', installer_source: '') }
       .to raise_error(Puppet::Error, %r{Installer source must be a valid URL})
   end
 
   it 'fails with a non-URL installer_source' do
-    expect { described_class.new(name: 'user', installer_source: 's a as') }
+    expect { described_class.new(title: 'user', installer_source: 's a as') }
       .to raise_error(Puppet::Error, %r{Installer source must be a valid URL})
   end
 
   it 'works with a URL installer_source' do
-    expect(described_class.new(name: 'user', installer_source: 'http://localhost/foo'))
+    expect(described_class.new(title: 'user', installer_source: 'http://localhost/foo'))
       .not_to be_nil
   end
 end
