a4fa3a5712248775d5b364bc50743eccd259bb76
Daniel Parks <dp+git@oxidized.org>
2022-09-06 20:44:42 -0700

    Put `PuppetX::Rustup` module definition in a separate file to support adding more submodules.

---
+++ lib/puppet_x/rustup.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'puppet_x'
+
+# Support module for rustup
+module PuppetX::Rustup; end
--- lib/puppet_x/rustup/util.rb
+++ lib/puppet_x/rustup/util.rb
@@ -1,33 +1,30 @@
 # frozen_string_literal: true
 
-require 'puppet_x'
+require_relative '../rustup'
 
-# Support module for rustup
-module PuppetX::Rustup
-  # Utility functions for rustup
-  module Util
-    # Remove a line from a file.
-    #
-    # This does the removal in place to avoid problems with symlinks, hard
-    # links, and preserving metadata (like modes and ACLs). This means the
-    # removal is not atomic.
-    #
-    # This only writes to the file if changes are made.
-    #
-    # line_to_remove is a string that matches the line to remove, not including
-    # newlines.
-    def self.remove_file_line(path, line_to_remove)
-      matcher = Regexp.escape(line_to_remove)
-      contents = IO.read(path)
-      if contents.gsub!(%r{^#{matcher}(\n|\r\n|\r|\Z)}, '')
-        # Substitutions were made
-        IO.write(path, contents)
-      end
+# Utility functions for rustup
+module PuppetX::Rustup::Util
+  # Remove a line from a file.
+  #
+  # This does the removal in place to avoid problems with symlinks, hard
+  # links, and preserving metadata (like modes and ACLs). This means the
+  # removal is not atomic.
+  #
+  # This only writes to the file if changes are made.
+  #
+  # line_to_remove is a string that matches the line to remove, not including
+  # newlines.
+  def self.remove_file_line(path, line_to_remove)
+    matcher = Regexp.escape(line_to_remove)
+    contents = IO.read(path)
+    if contents.gsub!(%r{^#{matcher}(\n|\r\n|\r|\Z)}, '')
+      # Substitutions were made
+      IO.write(path, contents)
     end
+  end
 
-    # Check that input is a non-empty string.
-    def self.non_empty_string?(input)
-      input.is_a?(String) && !input.empty?
-    end
+  # Check that input is a non-empty string.
+  def self.non_empty_string?(input)
+    input.is_a?(String) && !input.empty?
   end
 end
