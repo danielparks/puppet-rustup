6a3b96d0a39af2dd8a3ce4d3b6ea6e63bf7bed36
Daniel Parks <dp+git@oxidized.org>
2023-04-15 21:36:22 -0700

    Indent fix

--- lib/puppet_x/rustup/provider/toolchain.rb
+++ lib/puppet_x/rustup/provider/toolchain.rb
@@ -3,78 +3,76 @@
 require_relative '../provider'
 
 # A toolchain subresource
-module PuppetX::Rustup::Provider
-  class Toolchain
-    class << self
-      def property(name)
-        attr_accessor(name)
-        @properties ||= []
-        @properties << name
-      end
-
-      def parameter(name)
-        attr_accessor(name)
-        @parameters ||= []
-        @parameters << name
-      end
-
-      def properties
-        @properties || []
-      end
-
-      def parameters
-        @parameters || []
-      end
+class PuppetX::Rustup::Provider::Toolchain
+  class << self
+    def property(name)
+      attr_accessor(name)
+      @properties ||= []
+      @properties << name
     end
 
-    property :name
-    property :ensure
-    parameter :profile
-    parameter :normalized_name
-
-    def initialize(**kwargs)
-      kwargs.each do |key, value|
-        self[key] = value
-      end
+    def parameter(name)
+      attr_accessor(name)
+      @parameters ||= []
+      @parameters << name
     end
 
-    def to_h
-      h = {}
-      self.class.properties.each do |key|
-        h[key] = send(key)
-      end
-      # FIXME: parameters
-      h
+    def properties
+      @properties || []
     end
 
-    # Validate members against the property and parameter list rather than just
-    # trying to `send()` to avoid being able to call any function ending with
-    # "=" just by passing the right hash to the constructor.
-    def valid_member?(key)
-      key = key.to_sym
-      self.class.properties.include?(key) || self.class.parameters.include?(key)
+    def parameters
+      @parameters || []
     end
+  end
 
-    def validate_member(key)
-      unless valid_member?(key)
-        raise NameError, "invalid member #{key.inspect}"
-      end
-    end
+  property :name
+  property :ensure
+  parameter :profile
+  parameter :normalized_name
 
-    def [](key)
-      validate_member(key)
-      send(key)
+  def initialize(**kwargs)
+    kwargs.each do |key, value|
+      self[key] = value
     end
+  end
 
-    def []=(key, value)
-      validate_member(key)
-      send("#{key}=", value)
+  def to_h
+    h = {}
+    self.class.properties.each do |key|
+      h[key] = send(key)
     end
+    # FIXME: parameters
+    h
+  end
 
-    def self.from_system(name)
-      toolchain = new(name: name, ensure: 'present')
-      toolchain.normalized_name = name
-      toolchain
+  # Validate members against the property and parameter list rather than just
+  # trying to `send()` to avoid being able to call any function ending with
+  # "=" just by passing the right hash to the constructor.
+  def valid_member?(key)
+    key = key.to_sym
+    self.class.properties.include?(key) || self.class.parameters.include?(key)
+  end
+
+  def validate_member(key)
+    unless valid_member?(key)
+      raise NameError, "invalid member #{key.inspect}"
     end
+  end
+
+  def [](key)
+    validate_member(key)
+    send(key)
+  end
+
+  def []=(key, value)
+    validate_member(key)
+    send("#{key}=", value)
+  end
+
+  def self.from_system(name)
+    toolchain = new(name: name, ensure: 'present')
+    toolchain.normalized_name = name
+    toolchain
   end
 end
